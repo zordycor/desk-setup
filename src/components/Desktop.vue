a446e1d735dd5d8a7247a01888b726fa40b1ac5d
Borja Rodrigo <zordycorak@gmail.com>
2020-05-14 19:16:51 +0200

    First setup

---
+++ src/components/Desktop.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-img src="http://www.fillmurray.com/200/300"></v-img>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/DesktopList.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <Desktop />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Desktop from './Desktop'
+export default {
+  name: 'DesktopList',
+
+  components: {
+    Desktop
+  }
+}
+</script>
